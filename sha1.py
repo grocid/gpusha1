6f686843f314c32f54b174a0c70a9b76973da10c
grocid <carl.londahl@gmail.com>
2017-03-28 22:09:25 +0200

    fixed

--- sha1.py
+++ sha1.py
@@ -2,13 +2,11 @@ import os, sys, unittest, binascii
 import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
-import numpy
 import array, time, math, numpy
 
 class SHAgpu:
 
     cuda_inited = False
-    
     threadMax = 1024
     blockMax = 1024
     cuda_buf_size =  threadMax
@@ -28,13 +26,11 @@ class SHAgpu:
     def hex_encode(self, str):
     	return ''.join(hex(ord(c)) + ',' for c in str)[:-1]
 
- #if ((buf[19-0] == 0xff) && (buf[19-1] == 0xff) && (buf[19-2] == 0xff) && (buf[19-3] & 0x03 == 0x03) )
     def generate_mask_function(self):
     	full = self.set_bits / 8
     	rem = self.set_bits % 8
     	L = ['(buf[' + str(19-i) + '] == 0xff)' for i in range(0, full)]
     	R = ['(buf[' + str(19-full) + '] == ' + hex(2**rem - 1) + ')']
-
     	return '&&'.join(L + R)
 
     def init_cuda(self, prefix):
@@ -42,25 +38,22 @@ class SHAgpu:
             return
 
         f = open('sha1.c')
-
         cuda_kernel = f.read()
-        cuda_kernel = cuda_kernel.replace('__MAX_ITERATIONS__', 
+        cuda_kernel = cuda_kernel.replace('__MAX_ITERATIONS__',
         			str(len(self.alphabet) ** self.length))
-        cuda_kernel = cuda_kernel.replace('__ALPHABET__', 
+        cuda_kernel = cuda_kernel.replace('__ALPHABET__',
         			self.hex_encode(self.alphabet))
         cuda_kernel = cuda_kernel.replace('__LENGTH__', str(self.length))
         cuda_kernel = cuda_kernel.replace('__LOG2ALPHABET__', str(self.loglen))
         cuda_kernel = cuda_kernel.replace('__MASK__', str(self.alphalen - 1))
-        cuda_kernel = cuda_kernel.replace('__PREFIX__', 
+        cuda_kernel = cuda_kernel.replace('__PREFIX__',
         			self.hex_encode(prefix + '\x00' * (self.length + 1)))
         cuda_kernel = cuda_kernel.replace('__PREFIXLEN__', str(len(prefix)))
         cuda_kernel = cuda_kernel.replace('__MASKFUNC__', self.generate_mask_function())
-        print cuda_kernel
         f.close()
+
         self.mod = SourceModule(cuda_kernel)
-
         self.cuda_buf = cuda.mem_alloc(self.cuda_buf_size)
-
         self.cuda_inited = True
 
 
@@ -68,13 +61,8 @@ class SHAgpu:
     	print 'Running grocollv1 on 2^%s candidates...' % str(self.loglen * self.length)
     	print '-' * 30
         self.init_cuda('mothafucka')
-        
         run = self.mod.get_function("run");
-        
         run(self.cuda_buf, block = (1024, 1, 1), grid = (256, 1))
-
-        
-
 
 if __name__ == "__main__":
     hasher = SHAgpu()
